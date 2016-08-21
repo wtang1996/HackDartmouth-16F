5a661692048adabb247e45edc6a111c8cb9e3bc9
Weijia Tang <weijia.tang.18@dartmouth.edu>
2016-08-21 14:45:57 -0400

    author link

--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -1,9 +1,11 @@
 import { ActionTypes } from '../actions';
 
-const UserReducer = (state = { user: null }, action) => {
+const UserReducer = (state = { user: null, author: null }, action) => {
   switch (action.type) {
     case ActionTypes.FETCH_USER:
       return { ...state, user: action.payload.user };
+    case ActionTypes.FETCH_AUTHOR:
+      return { ...state, author: action.payload.author };
     default:
       return state;
   }
